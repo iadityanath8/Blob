6fee1aafc4e0f42fb223b3f5cfce186eb519b8d3
iadityanath8 <iadityanath8@gmail.com>
2026-04-05 16:16:58 +0530

    added

--- blob.h
+++ blob.h
@@ -12,11 +12,27 @@
 #define BLOB_DECL static inline
 #endif
 
+#ifndef STRICT_MODE 
+#define STRICT_MODE 0
+#endif 
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+
+#define da_append(xs, x)                                                             \
+  do {                                                                             \
+    if ((xs)->count >= (xs)->capacity) {                                         \
+      if ((xs)->capacity == 0) (xs)->capacity = 256;                           \
+      else (xs)->capacity *= 2;                                                \
+      (xs)->items = realloc((xs)->items, (xs)->capacity*sizeof(*(xs)->items)); \
+    }                                                                            \
+    \
+    (xs)->items[(xs)->count++] = (x);                                            \
+  } while (0)
+
 
 typedef uint8_t u8;
 typedef uint16_t u16;
@@ -32,15 +48,25 @@ typedef char* str;
 
 
 typedef enum {
-    B_MATCHED,
-    B_UNMATCHED,
-    B_SYNTAX_ERROR_RANGE_NO_END,
-    B_SYNTAX_ERROR_RANGE_NO_START,
-    B_SYNTAX_ERROR_WRG_RANGE,
-    B_SYNTAX_ERROR_UNCLOSED,
-    B_SYNTAX_NEGATE_ERR
+  B_MATCHED,
+  B_UNMATCHED,
+  B_SYNTAX_ERROR_RANGE_NO_END,
+  B_SYNTAX_ERROR_RANGE_NO_START,
+  B_SYNTAX_ERROR_WRG_RANGE,
+  B_SYNTAX_ERROR_UNCLOSED,
+  B_SYNTAX_NEGATE_ERR
 }B_Result;
 
+typedef struct {
+  str  v;
+  u32 len;
+}Blob_String_View;
+
+typedef struct {
+  Blob_String_View* items;
+  i32 count;
+  i32 capacity;
+}String_Arr;
 
 const char* b_to_str(B_Result res){
   switch(res){
@@ -55,105 +81,219 @@ const char* b_to_str(B_Result res){
   }
 }
 
-
 /**
  *
  * string pattern match 
  * only matches the pattern to the text based
  * on the rule of glob in unix 1970 
+ * with strict version matches full text 
  */
-BLOB_DECL B_Result l_blob(str pattern, str text){
+BLOB_DECL B_Result BLB_blob(str patt, str tex){
   str star_pat = NULL;
   str text_pos = NULL;
+  str pattern = patt;
+  str text = tex;
+
   while (*text!='\0'){
     switch(*pattern){
-            case '?':{
-                pattern++;
-                text++;       
-            }break;
-            case '*':{
-               star_pat = ++pattern;
-               text_pos = text;
-           }break;
-            case '[':{
-                //str old_ptr = pattern++;
-                bool matcher = false;
-                bool negate = false;
-                while (*pattern != ']' && *pattern != '\0'){
-                    switch(*pattern){
-                        case '-':{
-                            // check for chars in here 
-                            if (pattern[1] == '\0' || pattern[1] == ']') return B_SYNTAX_ERROR_RANGE_NO_END;
-                            str bg = pattern - 1; // peek before 
-                            str ed = pattern + 1; // peek ahead
-                            if (*bg == '[')                              return B_SYNTAX_ERROR_RANGE_NO_START;
-                            if (*bg >= *ed)                              return B_SYNTAX_ERROR_WRG_RANGE;
-                            matcher |= (*bg <= *text && *text <= *ed);
-                        }break;
-                        case '!':{
-                          str prev = pattern - 1;
-                          str next = pattern + 1;
-                          if (*prev != '[')  {
-                            return B_SYNTAX_NEGATE_ERR;
-                          }
-                          if (*next == '\0') return B_SYNTAX_ERROR_UNCLOSED;
-                          if (*next == ']')  return B_SYNTAX_ERROR_RANGE_NO_END;
-                          negate=true;
-                        }break;         
-                        default:{   
-                            matcher |= (*pattern == *text);
-                        }break;
-                    }
-                    pattern++;
-                }
-                if (*pattern == '\0') return B_SYNTAX_ERROR_UNCLOSED;
-                if (negate) matcher = !matcher;
-                if (!matcher){
-                if (!star_pat) return B_UNMATCHED;
-                  pattern = star_pat;
-                  text = ++text_pos;
-                  continue;
-                } 
-                pattern++;
-                text++;
-            }break;
-            default:{
+      case '?':{
+                 pattern++;
+                 text++;       
+               }break;
+      case '*':{
+                 star_pat = ++pattern;
+                 text_pos = text;
+               }break;
+      case '[':{
+                 bool matcher = false;
+                 bool negate = false;
+                 while (*pattern != ']' && *pattern != '\0'){
+                   switch(*pattern){
+                     case '-':{
+                                // check for chars in here 
+                                if (pattern[1] == '\0' || pattern[1] == ']') return B_SYNTAX_ERROR_RANGE_NO_END;
+                                str bg = pattern - 1; // peek before 
+                                str ed = pattern + 1; // peek ahead
+                                if (*bg == '[')                              return B_SYNTAX_ERROR_RANGE_NO_START;
+                                if (*bg >= *ed)                              return B_SYNTAX_ERROR_WRG_RANGE;
+                                matcher |= (*bg <= *text && *text <= *ed);
+                              }break;
+                     case '!':{
+                                str prev = pattern - 1;
+                                str next = pattern + 1;
+                                if (*prev != '[')  {
+                                  return B_SYNTAX_NEGATE_ERR;
+                                }
+                                if (*next == '\0') return B_SYNTAX_ERROR_UNCLOSED;
+                                if (*next == ']')  return B_SYNTAX_ERROR_RANGE_NO_END;
+                                negate=true;
+                              }break;         
+                     default:{   
+                               matcher |= (*pattern == *text);
+                             }break;
+                   }
+                   pattern++;
+                 }
+                 if (*pattern == '\0') return B_SYNTAX_ERROR_UNCLOSED;
+                 if (negate) matcher = !matcher;
+                 if (!matcher){
+                   if (!star_pat) return B_UNMATCHED;
+                   pattern = star_pat;
+                   text = ++text_pos;
+                   continue;
+                 } 
+                 pattern++;
+                 text++;
+               }break;
+      default:{
                 if (*pattern == *text){
-                    pattern++;
-                    text++;
+                  pattern++;
+                  text++;
                 }else{
                   pattern = star_pat;
                   text = ++text_pos;
                   if (!star_pat) return B_UNMATCHED;
                 }
-            }break;
-        }
+              }break;
     }
+  }
 
-    if (*text == '\0'){
-        while (*pattern == '*') pattern++;
-        if (*pattern == '\0') return B_MATCHED;
-    }
-    return B_UNMATCHED;
+  if (*text == '\0'){
+    while (*pattern == '*') pattern++;
+    if (*pattern == '\0') return B_MATCHED;
+  }
+  return B_UNMATCHED;
 }
 
 
 /**
- *   using tables to minimize the recursion as minumum as possible
- *   two chocices of backtracking (+1)
- *   *a       GROKb, Meowa, Jefa
- *     
- *      p e a
- *   *  t t t
- *   a  f f t
+ *  
+ *  Strict Version of BLOB which does something like   
+ *  For matching whole text only use when in bmatch
  */
-BLOB_DECL B_Result bmatch(str pattern, str text){
-  // dp approach 
-  unused(pattern);
-  unused(text);
-  return B_MATCHED;
+BLOB_DECL B_Result BLB_Sblob(str pattern, str text,i32 n,i32 m,u32* n_matched){
+  i32 star_pat = -1;
+  i32 text_pos = -1;
+  i32 i = 0;
+  i32 j = 0;
+
+  while (j<m){
+    switch(pattern[i]){
+      case '?':{
+                 i++;
+                 j++;
+               }break;
+      case '*':{
+                 i++;
+                 star_pat = i;//pattern + i;
+                 text_pos = j;//text;
+               }break;
+      case '[':{
+                 bool matcher = false;
+                 bool negate = false;
+                 while (i < n && pattern[i] != ']'){
+                   switch(pattern[i]){
+                     case '-':{
+                                if (i + 1 == n || pattern[i + 1] == ']') return B_SYNTAX_ERROR_RANGE_NO_END;
+                                str bg = (pattern + i) - 1; // peek before 
+                                str ed = (pattern + i) + 1; // peek ahead  range_start
+                                if (*bg == '[')                              return B_SYNTAX_ERROR_RANGE_NO_START;
+                                if (*bg >= *ed)                              return B_SYNTAX_ERROR_WRG_RANGE;
+                                matcher |= (*bg <= text[j] && text[j] <= *ed);
+                              }break;
+                     case '!':{
+                                str prev = pattern + i - 1;
+                                str next = pattern + i + 1;
+                                if (*prev != '[')  {
+                                  return B_SYNTAX_NEGATE_ERR;
+                                }
+                                if ((i + 1) == n) return B_SYNTAX_ERROR_UNCLOSED;
+                                if (*next == ']')  return B_SYNTAX_ERROR_RANGE_NO_END;
+                                negate=true;
+                              }break;         
+                     default:{   
+                               matcher |= (pattern[i] == text[j]);
+                             }break;
+                   }
+                   i++;
+                 }
+                 if (i >= n) return B_SYNTAX_ERROR_UNCLOSED;
+                 if (negate) matcher = !matcher;
+                 if (!matcher){
+                   if (star_pat == -1) return B_UNMATCHED;
+                   i =  star_pat;
+                   j = ++text_pos;
+                   continue;
+                 }
+                 i++;
+                 j++;
+               }break;
+      default:{
+                if (pattern[i] == text[j]){
+                  i++;
+                  j++;
+                }else{
+                  if (star_pat == -1) return B_UNMATCHED;
+                  i = star_pat;
+                  j = ++text_pos;
+                }
+              }break;
+    }
+    if (i == n) {
+      if (n_matched) *n_matched = j;
+      return B_MATCHED;
+    }
+  }
+
+  /** strict Mode will be removed in indexed version **/
+#if 1 
+  if (j == m){
+    while (i < n && pattern[i] == '*') i++;
+    if (i == n) {
+      if (n_matched) *n_matched = j;
+      return B_MATCHED;
+    }
+  }
+#endif 
+  return B_UNMATCHED;
 }
 
+
+
+
+/**
+  function returns the Arr of String_View of the particular string matchied 
+
+  typedef struct {
+    str  v;
+    u32 len;
+  }Blob_String_View;
+*/
+
+/*
+ *        Blob_String_View view = {.v = text + pos,.len = (u32)(n_matched)};
+          da_append(&arr,view);
+ *
+ */
+
+BLOB_DECL void BLB_free_Arr(String_Arr arr) {
+  free(arr.items); 
+}
+
+BLOB_DECL String_Arr BLB_match(str pattern, str text){
+  String_Arr arr = {0};
+  u32 n = strlen(pattern);
+  u32 m = strlen(text);
+
+  u32 n_matched = 0;
+  for (u32 pos = 0;pos < m;pos++) {
+    if (BLB_Sblob(pattern,text + pos,n,m - pos,&n_matched) == B_MATCHED) {
+      Blob_String_View view = {.v = text + pos,.len = (n_matched)};
+      da_append(&arr,view);
+    }
+  }
+  return arr;
+}
 
 
 
--- main.c
+++ main.c
@@ -6,39 +6,58 @@
 #define auto __auto_type
 
 void expect(char *pattern, char *text, B_Result expected,
-            const char *file, int line) {
+    const char *file, int line) {
 
-    auto result = l_blob(pattern, text);
-    
-    if (result != expected) {
-        printf("%s:%d: expectation failed\n", file, line);
-        printf("pattern: %s\n", pattern);
-        printf("text: %s\n", text);
-        printf("expected: %d got: %d\n", expected, result);
-    }else{
-        printf("Test case Passed successfully\n");
-    }
+  int n = strlen(pattern);
+  int m = strlen(text);
+  auto result = blob_indexed(pattern, text,n,m,NULL);
+
+  if (result != expected) {
+    printf("%s:%d: expectation failed\n", file, line);
+    printf("pattern: %s\n", pattern);
+    printf("text: %s\n", text);
+    printf("expected: %d got: %d\n", expected, result);
+  }else{
+    printf("Test case Passed successfully\n");
+  }
 }
 
 int main() {
-    EXPECT("?MEo","bMEo",     B_MATCHED);    
-    EXPECT("main.?","main.c", B_MATCHED);     
-    EXPECT("main.?","main.js",B_UNMATCHED);     
-    
-    printf("---------------------------------------------------------------\n");
-    EXPECT("maina*","mainajs",B_MATCHED);   
-    EXPECT("*Law*","GrokLaw", B_MATCHED);
-    EXPECT("*Law*", "Lawyer", B_MATCHED);
-    printf("---------------------------------------------------------------\n");
-    EXPECT("[ab]de", "cde",   B_UNMATCHED);
-    EXPECT("[ab]de","ade",    B_MATCHED);
-    printf("\n");
+//  EXPECT("?MEo","bMEo",     B_MATCHED);    
+//  EXPECT("main.?","main.c", B_MATCHED);     
+//  EXPECT("main.?","main.js",B_UNMATCHED);     
+//
+//  printf("---------------------------------------------------------------\n");
+//  EXPECT("*","",B_MATCHED);   
+//  EXPECT("*Law*","GrokLaw", B_MATCHED);
+//  EXPECT("*Law*", "Lawyer", B_MATCHED);
+//  printf("---------------------------------------------------------------\n");
+//  EXPECT("[ab]de", "cde",   B_UNMATCHED);
+//  EXPECT("[ab]de","ade",    B_MATCHED);
+//  printf("\n");
+//
+//  EXPECT("[0-9]de[a-z]","2dez",B_MATCHED);
+//  printf("---------------------------------------------------------------\n");
+//  EXPECT("[!a-d0-2]pop","epop",B_MATCHED);
+//
+//  EXPECT("?[a-f]*", "dfBHow", B_MATCHED); 
+//  EXPECT("[0-9]hosadi", "5hosadi", B_MATCHED);
 
-    EXPECT("[0-9]de[a-z]","2dez",B_MATCHED);
-    printf("---------------------------------------------------------------\n");
-    EXPECT("[!a-d0-2]pop","3pop",B_MATCHED);
-    
-    EXPECT("?[a-f]*", "dfBHow", B_MATCHED); 
-    EXPECT("*a", "thisa is londona", B_MATCHED);
-    return 0;
+  str s2 = "this 99th is me in here woow dfBHOW bBHOW";
+//  str pattern = "[0-9][0-9]thchar";
+  str patt2 = "[db][df?]BHOW";
+  auto arr = BLB_match(patt2, s2);
+
+  for (i32 i = 0;i < arr.count;i++){
+    printf("%.*s\n",arr.items[i].len,arr.items[i].v);
+  }
+  BLB_free_Arr(arr);
+  return 0;
 }
+
+
+
+
+
+
+
